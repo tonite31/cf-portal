bd2f32f1e5feffe2c137a11b7d0d27a039679648
Alprensia <tonite32@gmail.com>
2016-06-17 17:01:52 +0900

    Fixed bugs. Added column 'host' of application status.

--- modules/main/views/js/organization/app_status.js
+++ modules/main/views/js/organization/app_status.js
@@ -31,19 +31,35 @@
 					$(context).find('.status-table tbody').html('');
 					for(var key in result)
 					{
-						var days = Math.floor(result[key].stats.uptime / 60 / 60 / 24);
-						var remain = ((result[key].stats.uptime / 60 / 60 / 24) - days) * 24;
-						var hours = Math.floor(remain);
-						var min = Math.floor((remain - hours) * 60);
-						
+						console.log(result[key]);
 						var body = '';
 						body += '<tr>';
 						body += '<td>' + key + '</td>';
 						body += '<td>' + result[key].state + '</td>';
-						body += '<td>' + Math.round(result[key].stats.usage.cpu * 100) + '% </td>';
-						body += '<td>' + Math.round(result[key].stats.usage.mem / 1024 / 1024) + ' MB</td>';
-						body += '<td>' + Math.round(result[key].stats.usage.disk / 1024 / 1024) + ' MB</td>';
-						body += '<td>' + (days > 0 ? days + 'd ' : '') + (hours > 0 ? hours + 'hr ' : '') + (min > 0 ? min + 'min' : '') + '</td>';
+						
+						if(result[key].stats)
+						{
+							body += '<td>' + result[key].stats.host + '</td>';
+							
+							var days = Math.floor(result[key].stats.uptime / 60 / 60 / 24);
+							var remain = ((result[key].stats.uptime / 60 / 60 / 24) - days) * 24;
+							var hours = Math.floor(remain);
+							var min = Math.floor((remain - hours) * 60);
+							
+							body += '<td>' + Math.round(result[key].stats.usage.cpu * 100) + '% </td>';
+							body += '<td>' + Math.round(result[key].stats.usage.mem / 1024 / 1024) + ' MB</td>';
+							body += '<td>' + Math.round(result[key].stats.usage.disk / 1024 / 1024) + ' MB</td>';
+							body += '<td>' + (days > 0 ? days + 'd ' : '') + (hours > 0 ? hours + 'hr ' : '') + (min > 0 ? min + 'min' : '') + '</td>';
+						}
+						else
+						{
+							body += '<td></td>';
+							body += '<td></td>';
+							body += '<td></td>';
+							body += '<td></td>';
+							body += '<td></td>';
+						}
+						
 						body += '</tr>';
 						
 						$(context).find('.status-table tbody').append(body);
